6bc1082a2d2543d262fc492f40375e8a5169ce8f
shawnamonet <shawnampopejoy@gmail.com>
2026-08-13 16:46:57 -0700

    changes1

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-// 1. Grab the elements we care about
+console.log("JS file loaded!");
+
 const countryInput = document.querySelector('#country-input');
 const updateButton = document.querySelector('#update-btn');
 const countryHeading = document.querySelector('.country-card h2');
 
-updateButton.addEventListener('click', function () {
-  const newCountryName = countryInput.value;   // read the input
+async function updateCountryCard(countryName) {
+  console.log("Updating card for:", countryName);
+}
 
-  countryHeading.textContent = newCountryName; // update the <h2>
-
-  countryInput.value = '';      // clear the box
-  countryInput.focus();         // put cursor back in
+updateButton.addEventListener('click', () => {
+  const countryName = countryInput.value.trim();
+  updateCountryCard(countryName);
 });
